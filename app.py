7439e41ad0ab1755cfeca5b687f19db0744ad172
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-10 11:20:44 -0600

    you can now post a new todo at /todo

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Todo(db.Model):
+    __tablename__="todo"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean)
+
+    def __init__(self, title, done):
+        self.title = title
+        self.done = done
+    
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "done")
+
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/todo", methods=["POST"])
+def add_todo():
+    title = request.json["title"]
+    done = request.json["done"]
+
+    new_todo = Todo(title, done)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    todo = Todo.query.get(new_todo.id)
+    return todo_schema.jsonify(todo)
 
 if __name__ == "__main__":
     app.debug = True
